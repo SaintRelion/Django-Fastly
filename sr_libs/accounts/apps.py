096c311f352e31a34694f7ae078f1198173c0511
SaintRelion <saintrelion@gmail.com>
2026-02-23 18:55:22 +0800

    apps.py, removed installed apps auto inject

--- sr_libs/accounts/apps.py
+++ sr_libs/accounts/apps.py
@@ -4,14 +4,3 @@ from django.conf import settings
 
 class AccountsConfig(AppConfig):
     name = "sr_libs.accounts"
-
-    def ready(self):
-        required_apps = [
-            "rest_framework",
-            "rest_framework_simplejwt",
-            "rest_framework_simplejwt.token_blacklist",
-        ]
-
-        for app in required_apps:
-            if app not in settings.INSTALLED_APPS:
-                settings.INSTALLED_APPS.append(app)
