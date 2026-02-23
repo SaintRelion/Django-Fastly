c7138bfed16bec4795309ecafdbd7a7dcfecf6c6
SaintRelion <saintrelion@gmail.com>
2026-02-23 19:41:41 +0800

    fix settings v3

--- sr_libs/accounts/settings.py
+++ sr_libs/accounts/settings.py
@@ -20,12 +20,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    # May transfer to DAL
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filters.OrderingFilter",
-        "rest_framework.filters.SearchFilter",
-    ],
-    "DEFAULT_PAGINATION_CLASS": "core.pagination.DefaultPagination",
-    "PAGE_SIZE": 25,
+    # # May transfer to DAL
+    # "DEFAULT_FILTER_BACKENDS": [
+    #     "django_filters.rest_framework.DjangoFilterBackend",
+    #     "rest_framework.filters.OrderingFilter",
+    #     "rest_framework.filters.SearchFilter",
+    # ],
+    # "DEFAULT_PAGINATION_CLASS": "core.pagination.DefaultPagination",
+    # "PAGE_SIZE": 25,
 }
