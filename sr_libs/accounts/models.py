123bf8d13397727741d60e9c40c582a75f4d2da7
SaintRelion <saintrelion@gmail.com>
2026-02-23 19:11:20 +0800

    trying to fix user models

--- sr_libs/accounts/models.py
+++ sr_libs/accounts/models.py
@@ -1,9 +1,26 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
 class User(AbstractUser):
     extra_info = models.JSONField(default=dict, blank=True)
+
+    # override reverse accessors
+    groups = models.ManyToManyField(
+        Group,
+        related_name="sr_accounts_users",  # unique
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="sr_accounts_users_permissions",  # unique
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
 
     def __str__(self):
         return self.username
