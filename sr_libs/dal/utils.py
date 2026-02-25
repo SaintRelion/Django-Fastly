1463b196b19aebfca9c7b29e0374f6104e8e6767
SaintRelion <saintrelion@gmail.com>
2026-02-26 02:56:28 +0800

    fixing dal filter v1

--- sr_libs/dal/utils.py
+++ sr_libs/dal/utils.py
@@ -10,11 +10,6 @@ def apply_dynamic_filters(qs, model, query_params):
 
         try:
             field = model._meta.get_field(key)
-
-            # Convert FK to underlying id field
-            if isinstance(field, models.ForeignKey):
-                key = f"{key}_id"
-
         except FieldDoesNotExist:
             # Allow lookup expressions like due_date__gte
             base_field = key.split("__")[0]
@@ -25,6 +20,14 @@ def apply_dynamic_filters(qs, model, query_params):
                 continue  # skip invalid fields
 
         # Handle comma-separated values (__in)
+        if isinstance(field, models.ForeignKey):
+            key = f"{key}_id"
+            value = int(value)  # ensure integer
+
+        elif isinstance(field, models.CharField):
+            filter_kwargs[f"{key}__iexact"] = value  # case-insensitive
+            continue  # skip the exact filter below
+
         if "," in value:
             filter_kwargs[f"{key}__in"] = value.split(",")
         else:
