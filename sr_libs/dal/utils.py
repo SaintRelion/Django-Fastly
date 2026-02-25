a805d9b0076500dff1340bdbbe2ba98bfbf81bcb
SaintRelion <saintrelion@gmail.com>
2026-02-26 03:23:18 +0800

    fixed filter v7

--- sr_libs/dal/utils.py
+++ sr_libs/dal/utils.py
@@ -5,17 +5,21 @@ from django.db import models
 def apply_dynamic_filters(qs, model, query_params):
     filter_kwargs = {}
     print(query_params)
+
     for key, value in query_params.items():
+        # If value is a list, take the first element
+        if isinstance(value, list):
+            value = value[0]
+
         try:
             field = model._meta.get_field(key)
         except FieldDoesNotExist:
             continue  # skip unknown fields
 
-        # If it's a FK, filter by underlying _id
+        # ForeignKey -> use _id
         if isinstance(field, models.ForeignKey):
             key = f"{key}_id"
 
-        # Use value as-is (string), exact match
         filter_kwargs[key] = value
 
     if filter_kwargs:
