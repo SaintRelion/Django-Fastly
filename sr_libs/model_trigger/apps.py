6724b7a81e58d294bb523cb9656c8eaede28df9a
SaintRelion <saintrelion@gmail.com>
2026-02-26 12:35:14 +0800

    fixing celery v4

--- sr_libs/model_trigger/apps.py
+++ sr_libs/model_trigger/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class ModelTriggerConfig(AppConfig):
     name = "sr_libs.model_trigger"
-
-    def ready(self):
-        from .signals import setup_model_signals
-
-        setup_model_signals()
