b748ec8730fc08072d3712994b89b549cc8e61be
SaintRelion <saintrelion@gmail.com>
2026-02-28 17:33:35 +0800

    fxiing v11

--- sr_libs/authentication/urls.py
+++ sr_libs/authentication/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", MeView.as_view(), name="me"),
-    path("check/device", CheckDeviceView.as_view(), name="check_device"),
+    path("check/device/", CheckDeviceView.as_view(), name="check_device"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
