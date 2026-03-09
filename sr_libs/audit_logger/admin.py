7f7962d07845f7bc627c0ec995c845aab32f64ef
SaintRelion <saintrelion@gmail.com>
2026-03-09 11:03:00 +0800

    fixed admin for audit

--- sr_libs/audit_logger/admin.py
+++ sr_libs/audit_logger/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
 from .models import AuditLog
 
+
 @admin.register(AuditLog)
 class AuditLogAdmin(admin.ModelAdmin):
-    list_display = ("action","model_name","object_id","user","source","created_at")
-    list_filter = ("action","source","model_name")
-    search_fields = ("model_name","object_id","user__username")
+
+    list_display = (
+        "action",
+        "category",
+        "object_id",
+        "user",
+        "source",
+        "ip_address",
+        "created_at",
+    )
+
+    list_filter = (
+        "action",
+        "source",
+        "category",
+        "created_at",
+    )
+
+    search_fields = (
+        "category",
+        "object_id",
+        "user__username",
+        "ip_address",
+    )
+
     readonly_fields = [f.name for f in AuditLog._meta.fields]
+
+    ordering = ("-created_at",)
+
+    date_hierarchy = "created_at"
